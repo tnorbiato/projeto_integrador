62ce359c3ac0cb23b0480529eaa49c6fcab2ba85
Tamiris Norbiato <tamiris.norbiato@outlook.com>
2023-07-11 20:23:36 -0300

    alterações em javascript

---
+++ javascript/index.js
@@ -0,0 +1,39 @@
+let cartazFilme4 = document.getElementById("cartaz-filme4");
+cartazFilme4.innerHTML = `
+<li id="cartaz-filme4">
+    <a href="https://www.youtube.com/watch?v=BydjQP2aFd0&pp=ygUPZWxlbWVudG9zIGZpbG1l"> 
+    <img src="/img/filmes-index/elementos.jpg" width=200px alt="Cartaz do filme Elementos">
+    <h4>Elementos</h4>
+    </a>
+</li>
+`
+
+let breveFilme1 = document.getElementById("breve-filme1");
+breveFilme1.innerHTML = `
+<li id="breve-filme1">
+    <a href="https://www.youtube.com/watch?v=F3OxA9Cz17A&pp=ygUTdHJhaWxlciBvcHBlbmhlaW1lcg%3D%3D"> 
+    <img src="/img/filmes-index/oppenheimer-novo-poster.jpg" width=200px alt="Cartaz do filme Oppenheimer">
+    <h4>Oppenheimer</h4>
+    </a>
+</li>
+`
+
+let cartazFilme5 = document.getElementById("cartaz-filme5");
+cartazFilme5.innerHTML = `
+<li id="cartaz-filme5">
+    <a href="https://www.youtube.com/watch?v=e8-GtGmlu18&pp=ygUdc29icmVuYXR1cmFsIGEgcG9ydGEgdmVybWVsaGE%3D">
+    <img src="/img/filmes-index/sobrenatural-a-porta-vermelha.jpg" width=200px alt="Cartaz do filme Sobrenatural: A Porta Vermelha">
+    <h4>Sobrenatural: A Porta Vermelha</h4>
+    </a>
+</li>
+`
+
+let breveFilme2 = document.getElementById("breve-filme2");
+breveFilme2.innerHTML = `
+<li id="breve-filme2">
+    <a href="https://www.youtube.com/watch?v=Xzaw65HsT54&pp=ygUQbG91Y2FzIGVtIGFwdXJvcw%3D%3D"> 
+    <img src="/img/filmes-index/LOUCAS-EM-APUROS.webp" width=200px alt="Cartaz do filme Loucas em Apuros">
+    <h4>Loucas em Apuros</h4>
+    </a>
+</li>
+`
---
+++ javascript/padrao.js
@@ -0,0 +1,225 @@
+const slide1 = document.createElement("div");
+
+slide1.innerHTML = `
+        <div class="slide-track">
+                <div class="slide">
+                <img src="/img/background/1.gif"/>
+                </div>
+                <div class="slide">
+                <img src="/img/background/2.gif"/>
+                </div>
+                <div class="slide">
+                <img src="/img/background/3.gif"/>
+                </div>
+                <div class="slide">
+                <img src="/img/background/4.gif"/>
+                </div>
+                <div class="slide">
+                <img src="/img/background/7.gif"/>
+                </div>
+                <div class="slide">
+                <img src="/img/background/8.gif"/>
+                </div>
+                <div class="slide">
+                <img src="/img/background/9.gif"/>
+                </div>
+                <div class="slide">
+                <img src="/img/background/11.gif"/>
+                </div>
+                <div class="slide">
+                <img src="/img/background/12.gif"/>
+                </div>
+                <div class="slide">
+                <img src="/img/background/13.gif"/>
+                </div>
+                <div class="slide">
+                <img src="/img/background/14.gif"/>
+                </div>
+                <div class="slide">
+                <img src="/img/background/15.gif"/>
+                </div>
+                <div class="slide">
+                <img src="/img/background/16.gif"/>
+                </div>
+                <div class="slide">
+                <img src="/img/background/17.gif"/>
+                </div>
+                <div class="slide">
+                <img src="/img/background/18.gif"/>
+                </div>
+                <div class="slide">
+                <img src="/img/background/19.gif"/>
+                </div>
+                <div class="slide">
+                <img src="/img/background/20.gif"/>
+                </div>
+
+                <div class="slide">
+                <img src="/img/background/1.gif"/>
+                </div>
+                <div class="slide">
+                <img src="/img/background/2.gif"/>
+                </div>
+                <div class="slide">
+                <img src="/img/background/3.gif"/>
+                </div>
+                <div class="slide">
+                <img src="/img/background/4.gif"/>
+                </div>
+                <div class="slide">
+                <img src="/img/background/7.gif"/>
+                </div>
+                <div class="slide">
+                <img src="/img/background/8.gif"/>
+                </div>
+                <div class="slide">
+                <img src="/img/background/9.gif"/>
+                </div>
+                <div class="slide">
+                <img src="/img/background/11.gif"/>
+                </div>
+                <div class="slide">
+                <img src="/img/background/12.gif"/>
+                </div>
+                <div class="slide">
+                <img src="/img/background/13.gif"/>
+                </div>
+                <div class="slide">
+                <img src="/img/background/14.gif"/>
+                </div>
+                <div class="slide">
+                <img src="/img/background/15.gif"/>
+                </div>
+                <div class="slide">
+                <img src="/img/background/16.gif"/>
+                </div>
+                <div class="slide">
+                <img src="/img/background/17.gif"/>
+                </div>
+                <div class="slide">
+                <img src="/img/background/18.gif"/>
+                </div>
+                <div class="slide">
+                <img src="/img/background/19.gif"/>
+                </div>
+                <div class="slide">
+                <img src="/img/background/20.gif"/>
+                </div>
+        </div>
+`
+
+let slider1 = document.querySelector(".slider1");
+slider1.appendChild(slide1);
+
+const slide2 = document.createElement("div");
+
+slide2.innerHTML = `
+        <div class="slide-track">
+                <div class="slide">
+                <img src="/img/background/1.gif"/>
+                </div>
+                <div class="slide">
+                <img src="/img/background/2.gif"/>
+                </div>
+                <div class="slide">
+                <img src="/img/background/3.gif"/>
+                </div>
+                <div class="slide">
+                <img src="/img/background/4.gif"/>
+                </div>
+                <div class="slide">
+                <img src="/img/background/7.gif"/>
+                </div>
+                <div class="slide">
+                <img src="/img/background/8.gif"/>
+                </div>
+                <div class="slide">
+                <img src="/img/background/9.gif"/>
+                </div>
+                <div class="slide">
+                <img src="/img/background/11.gif"/>
+                </div>
+                <div class="slide">
+                <img src="/img/background/12.gif"/>
+                </div>
+                <div class="slide">
+                <img src="/img/background/13.gif"/>
+                </div>
+                <div class="slide">
+                <img src="/img/background/14.gif"/>
+                </div>
+                <div class="slide">
+                <img src="/img/background/15.gif"/>
+                </div>
+                <div class="slide">
+                <img src="/img/background/16.gif"/>
+                </div>
+                <div class="slide">
+                <img src="/img/background/17.gif"/>
+                </div>
+                <div class="slide">
+                <img src="/img/background/18.gif"/>
+                </div>
+                <div class="slide">
+                <img src="/img/background/19.gif"/>
+                </div>
+                <div class="slide">
+                <img src="/img/background/20.gif"/>
+                </div>
+
+                <div class="slide">
+                <img src="/img/background/1.gif"/>
+                </div>
+                <div class="slide">
+                <img src="/img/background/2.gif"/>
+                </div>
+                <div class="slide">
+                <img src="/img/background/3.gif"/>
+                </div>
+                <div class="slide">
+                <img src="/img/background/4.gif"/>
+                </div>
+                <div class="slide">
+                <img src="/img/background/7.gif"/>
+                </div>
+                <div class="slide">
+                <img src="/img/background/8.gif"/>
+                </div>
+                <div class="slide">
+                <img src="/img/background/9.gif"/>
+                </div>
+                <div class="slide">
+                <img src="/img/background/11.gif"/>
+                </div>
+                <div class="slide">
+                <img src="/img/background/12.gif"/>
+                </div>
+                <div class="slide">
+                <img src="/img/background/13.gif"/>
+                </div>
+                <div class="slide">
+                <img src="/img/background/14.gif"/>
+                </div>
+                <div class="slide">
+                <img src="/img/background/15.gif"/>
+                </div>
+                <div class="slide">
+                <img src="/img/background/16.gif"/>
+                </div>
+                <div class="slide">
+                <img src="/img/background/17.gif"/>
+                </div>
+                <div class="slide">
+                <img src="/img/background/18.gif"/>
+                </div>
+                <div class="slide">
+                <img src="/img/background/19.gif"/>
+                </div>
+                <div class="slide">
+                <img src="/img/background/20.gif"/>
+                </div>
+        </div>
+`
+
+let slider2 = document.querySelector(".slider2");
+slider2.appendChild(slide2);
