071ebe80eb054e5ea03e0b2f8af1ddb79e07fadd
Tamiris Norbiato <tamiris.norbiato@outlook.com>
2023-07-18 04:13:50 -0300

    adição dos sliders no index e hover na nav e footer

--- javascript/index.js
+++ javascript/index.js
@@ -1,39 +1,216 @@
-let cartazFilme4 = document.getElementById("cartaz-filme4");
+let sliderCartaz = tns({
+  container : ".my-sliderCartaz",
+  "slideBy" : 1,
+  "speed" : 400,
+  "nav" : false,
+  controlsContainer : "#controlsCartaz",
+  prevButton : ".prevCartaz",
+  nextButton : ".nextCartaz",
+  responsive : {
+    1600: {
+      items : 5,
+      gutter : 10
+    },
+    1024: {
+      items : 4,
+      gutter : 10
+    },
+    768: {
+      items : 3,
+      gutter : 10
+    },
+    480: {
+      items : 2,
+      gutter : 10
+    },
+  }
+});
+
+let sliderBreve = tns({
+  container : ".my-sliderBreve",
+  "slideBy" : 1,
+  "speed" : 400,
+  "nav" : false,
+  controlsContainer : "#controlsBreve",
+  prevButton : ".prevBreve",
+  nextButton : ".nextBreve",
+  responsive : {
+    1600: {
+      items : 5,
+      gutter : 10
+    },
+    1024: {
+      items : 4,
+      gutter : 10
+    },
+    768: {
+      items : 3,
+      gutter : 10
+    },
+    480: {
+      items : 2,
+      gutter : 10
+    },
+  }
+});
+
+let cartazFilme4 = document.querySelector("#tns1-item3 > div");
 cartazFilme4.innerHTML = `
-<li id="cartaz-filme4">
-    <a href="https://www.youtube.com/watch?v=BydjQP2aFd0&pp=ygUPZWxlbWVudG9zIGZpbG1l"> 
-    <img src="/img/filmes-index/elementos.jpg" width=200px alt="Cartaz do filme Elementos">
-    <h4>Elementos</h4>
-    </a>
-</li>
+<div>
+  <div class="slide">
+    <div class="slide-img imgCartaz-4">
+      <a href="https://www.youtube.com/watch?v=BydjQP2aFd0&pp=ygURdHJhaWxlciBlbGVtZW50b3M%3D">Ver trailer</a>
+    </div>
+    <br>
+    <div>
+        <h3>Elementos</h3>
+    </div>
+  </div>
+</div>
 `
 
-let breveFilme1 = document.getElementById("breve-filme1");
-breveFilme1.innerHTML = `
-<li id="breve-filme1">
-    <a href="https://www.youtube.com/watch?v=F3OxA9Cz17A&pp=ygUTdHJhaWxlciBvcHBlbmhlaW1lcg%3D%3D"> 
-    <img src="/img/filmes-index/oppenheimer-novo-poster.jpg" width=200px alt="Cartaz do filme Oppenheimer">
-    <h4>Oppenheimer</h4>
-    </a>
-</li>
+let cartazFilme4Clone1 = document.querySelector("#tns1 > div:nth-child(20)");
+cartazFilme4Clone1.innerHTML = `
+<div>
+  <div class="slide">
+    <div class="slide-img imgCartaz-4">
+      <a href="https://www.youtube.com/watch?v=BydjQP2aFd0&pp=ygURdHJhaWxlciBlbGVtZW50b3M%3D">Ver trailer</a>
+    </div>
+    <br>
+    <div>
+        <h3>Elementos</h3>
+    </div>
+  </div>
+</div>
+`
+let cartazFilme4Clone2 = document.querySelector("#tns1 > div:nth-child(8)");
+cartazFilme4Clone2.innerHTML = `
+<div>
+  <div class="slide">
+    <div class="slide-img imgCartaz-4">
+      <a href="https://www.youtube.com/watch?v=BydjQP2aFd0&pp=ygURdHJhaWxlciBlbGVtZW50b3M%3D">Ver trailer</a>
+    </div>
+    <br>
+    <div>
+        <h3>Elementos</h3>
+    </div>
+  </div>
+</div>
 `
 
-let cartazFilme5 = document.getElementById("cartaz-filme5");
+let cartazFilme5 = document.querySelector("#tns1-item4 > div");
 cartazFilme5.innerHTML = `
-<li id="cartaz-filme5">
-    <a href="https://www.youtube.com/watch?v=e8-GtGmlu18&pp=ygUdc29icmVuYXR1cmFsIGEgcG9ydGEgdmVybWVsaGE%3D">
-    <img src="/img/filmes-index/sobrenatural-a-porta-vermelha.jpg" width=200px alt="Cartaz do filme Sobrenatural: A Porta Vermelha">
-    <h4>Sobrenatural: A Porta Vermelha</h4>
-    </a>
-</li>
+<div>
+  <div class="slide">
+    <div class="slide-img imgCartaz-5">
+        <a href="https://www.youtube.com/watch?v=e8-GtGmlu18&pp=ygUeU29icmVuYXR1cmFsOiBBIFBvcnRhIFZlcm1lbGhh">Ver trailer</a>
+    </div>
+    <br>
+    <div>
+        <h3>Sobrenatural: A Porta Vermelha</h3>
+    </div>
+  </div>
+</div>
 `
 
-let breveFilme2 = document.getElementById("breve-filme2");
-breveFilme2.innerHTML = `
-<li id="breve-filme2">
-    <a href="https://www.youtube.com/watch?v=Xzaw65HsT54&pp=ygUQbG91Y2FzIGVtIGFwdXJvcw%3D%3D"> 
-    <img src="/img/filmes-index/LOUCAS-EM-APUROS.webp" width=200px alt="Cartaz do filme Loucas em Apuros">
-    <h4>Loucas em Apuros</h4>
-    </a>
-</li>
+let cartazFilme5Clone1 = document.querySelector("#tns1 > div:nth-child(21)");
+cartazFilme5Clone1.innerHTML = `
+<div>
+  <div class="slide">
+    <div class="slide-img imgCartaz-5">
+        <a href="https://www.youtube.com/watch?v=e8-GtGmlu18&pp=ygUeU29icmVuYXR1cmFsOiBBIFBvcnRhIFZlcm1lbGhh">Ver trailer</a>
+    </div>
+    <br>
+    <div>
+        <h3>Sobrenatural: A Porta Vermelha</h3>
+    </div>
+  </div>
+</div>
 `
+
+let cartazFilme5Clone2 = document.querySelector("#tns1 > div:nth-child(9)");
+cartazFilme5Clone2.innerHTML = `
+<div>
+  <div class="slide">
+    <div class="slide-img imgCartaz-5">
+        <a href="https://www.youtube.com/watch?v=e8-GtGmlu18&pp=ygUeU29icmVuYXR1cmFsOiBBIFBvcnRhIFZlcm1lbGhh">Ver trailer</a>
+    </div>
+    <br>
+    <div>
+        <h3>Sobrenatural: A Porta Vermelha</h3>
+    </div>
+  </div>
+</div>
+`
+
+let cartazFilme6 = document.querySelector("#tns1-item5 > div");
+cartazFilme6.innerHTML = `
+<div>
+  <div class="slide">
+    <div class="slide-img imgCartaz-6">
+        <a href="https://www.youtube.com/watch?v=L8sDZ0EsQ4w&pp=ygUPcGVyZGlkYSB0cmFpbGVy">Ver trailer</a>
+    </div>
+    <br>
+    <div>
+        <h3>Perdida</h3>
+    </div>
+  </div>
+</div>
+`
+
+let cartazFilme6Clone1 = document.querySelector("#tns1 > div:nth-child(22)");
+cartazFilme6Clone1.innerHTML = `
+<div>
+  <div class="slide">
+    <div class="slide-img imgCartaz-6">
+        <a href="https://www.youtube.com/watch?v=L8sDZ0EsQ4w&pp=ygUPcGVyZGlkYSB0cmFpbGVy">Ver trailer</a>
+    </div>
+    <br>
+    <div>
+        <h3>Perdida</h3>
+    </div>
+  </div>
+</div>
+`
+
+let cartazFilme6Clone2 = document.querySelector("#tns1 > div:nth-child(10)");
+cartazFilme6Clone2.innerHTML = `
+<div>
+  <div class="slide">
+    <div class="slide-img imgCartaz-6">
+        <a href="https://www.youtube.com/watch?v=L8sDZ0EsQ4w&pp=ygUPcGVyZGlkYSB0cmFpbGVy">Ver trailer</a>
+    </div>
+    <br>
+    <div>
+        <h3>Perdida</h3>
+    </div>
+  </div>
+</div>
+`
+
+let imgCartaz4 = document.querySelector("#tns1-item3 > div > div > div > div.slide-img.imgCartaz-4");
+imgCartaz4.style.backgroundImage = "url('/img/filmes-index/elementos.jpg')";
+
+let imgCartaz5 = document.querySelector("#tns1-item4 > div > div > div > div.slide-img.imgCartaz-5");
+imgCartaz5.style.backgroundImage = "url('/img/filmes-index/sobrenatural-a-porta-vermelha.jpg')";
+
+let imgCartaz6 = document.querySelector("#tns1-item5 > div > div > div > div.slide-img.imgCartaz-6");
+imgCartaz6.style.backgroundImage = "url('/img/filmes-index/perdida.jpg')";
+
+let imgCartaz4Clone1 = document.querySelector("#tns1 > div:nth-child(20) > div > div > div.slide-img.imgCartaz-4");
+imgCartaz4Clone1.style.backgroundImage = "url('/img/filmes-index/elementos.jpg')";
+
+let imgCartaz5Clone1 = document.querySelector("#tns1 > div:nth-child(21) > div > div > div.slide-img.imgCartaz-5");
+imgCartaz5Clone1.style.backgroundImage = "url('/img/filmes-index/sobrenatural-a-porta-vermelha.jpg')";
+
+let imgCartaz6Clone1 = document.querySelector("#tns1 > div:nth-child(22) > div > div > div.slide-img.imgCartaz-6");
+imgCartaz6Clone1.style.backgroundImage = "url('/img/filmes-index/perdida.jpg')";
+
+let imgCartaz4Clone2 = document.querySelector("#tns1 > div:nth-child(8) > div > div > div.slide-img.imgCartaz-4");
+imgCartaz4Clone2.style.backgroundImage = "url('/img/filmes-index/elementos.jpg')";
+
+let imgCartaz5Clone2 = document.querySelector("#tns1 > div:nth-child(9) > div > div > div.slide-img.imgCartaz-5");
+imgCartaz5Clone2.style.backgroundImage = "url('/img/filmes-index/sobrenatural-a-porta-vermelha.jpg')";
+
+let imgCartaz6Clone2 = document.querySelector("#tns1 > div:nth-child(10) > div > div > div.slide-img.imgCartaz-6");
+imgCartaz6Clone2.style.backgroundImage = "url('/img/filmes-index/perdida.jpg')";
--- javascript/padrao.js
+++ javascript/padrao.js
@@ -2,107 +2,107 @@ const slide1 = document.createElement("div");
 
 slide1.innerHTML = `
         <div class="slide-track">
-                <div class="slide">
+                <div class="filme-bg">
                 <img src="/img/background/1.gif"/>
                 </div>
-                <div class="slide">
+                <div class="filme-bg">
                 <img src="/img/background/2.gif"/>
                 </div>
-                <div class="slide">
+                <div class="filme-bg">
                 <img src="/img/background/3.gif"/>
                 </div>
-                <div class="slide">
+                <div class="filme-bg">
                 <img src="/img/background/4.gif"/>
                 </div>
-                <div class="slide">
+                <div class="filme-bg">
                 <img src="/img/background/7.gif"/>
                 </div>
-                <div class="slide">
+                <div class="filme-bg">
                 <img src="/img/background/8.gif"/>
                 </div>
-                <div class="slide">
+                <div class="filme-bg">
                 <img src="/img/background/9.gif"/>
                 </div>
-                <div class="slide">
+                <div class="filme-bg">
                 <img src="/img/background/11.gif"/>
                 </div>
-                <div class="slide">
+                <div class="filme-bg">
                 <img src="/img/background/12.gif"/>
                 </div>
-                <div class="slide">
+                <div class="filme-bg">
                 <img src="/img/background/13.gif"/>
                 </div>
-                <div class="slide">
+                <div class="filme-bg">
                 <img src="/img/background/14.gif"/>
                 </div>
-                <div class="slide">
+                <div class="filme-bg">
                 <img src="/img/background/15.gif"/>
                 </div>
-                <div class="slide">
+                <div class="filme-bg">
                 <img src="/img/background/16.gif"/>
                 </div>
-                <div class="slide">
+                <div class="filme-bg">
                 <img src="/img/background/17.gif"/>
                 </div>
-                <div class="slide">
+                <div class="filme-bg">
                 <img src="/img/background/18.gif"/>
                 </div>
-                <div class="slide">
+                <div class="filme-bg">
                 <img src="/img/background/19.gif"/>
                 </div>
-                <div class="slide">
+                <div class="filme-bg">
                 <img src="/img/background/20.gif"/>
                 </div>
 
-                <div class="slide">
+                <div class="filme-bg">
                 <img src="/img/background/1.gif"/>
                 </div>
-                <div class="slide">
+                <div class="filme-bg">
                 <img src="/img/background/2.gif"/>
                 </div>
-                <div class="slide">
+                <div class="filme-bg">
                 <img src="/img/background/3.gif"/>
                 </div>
-                <div class="slide">
+                <div class="filme-bg">
                 <img src="/img/background/4.gif"/>
                 </div>
-                <div class="slide">
+                <div class="filme-bg">
                 <img src="/img/background/7.gif"/>
                 </div>
-                <div class="slide">
+                <div class="filme-bg">
                 <img src="/img/background/8.gif"/>
                 </div>
-                <div class="slide">
+                <div class="filme-bg">
                 <img src="/img/background/9.gif"/>
                 </div>
-                <div class="slide">
+                <div class="filme-bg">
                 <img src="/img/background/11.gif"/>
                 </div>
-                <div class="slide">
+                <div class="filme-bg">
                 <img src="/img/background/12.gif"/>
                 </div>
-                <div class="slide">
+                <div class="filme-bg">
                 <img src="/img/background/13.gif"/>
                 </div>
-                <div class="slide">
+                <div class="filme-bg">
                 <img src="/img/background/14.gif"/>
                 </div>
-                <div class="slide">
+                <div class="filme-bg">
                 <img src="/img/background/15.gif"/>
                 </div>
-                <div class="slide">
+                <div class="filme-bg">
                 <img src="/img/background/16.gif"/>
                 </div>
-                <div class="slide">
+                <div class="filme-bg">
                 <img src="/img/background/17.gif"/>
                 </div>
-                <div class="slide">
+                <div class="filme-bg">
                 <img src="/img/background/18.gif"/>
                 </div>
-                <div class="slide">
+                <div class="filme-bg">
                 <img src="/img/background/19.gif"/>
                 </div>
-                <div class="slide">
+                <div class="filme-bg">
                 <img src="/img/background/20.gif"/>
                 </div>
         </div>
@@ -115,107 +115,107 @@ const slide2 = document.createElement("div");
 
 slide2.innerHTML = `
         <div class="slide-track">
-                <div class="slide">
+                <div class="filme-bg">
                 <img src="/img/background/1.gif"/>
                 </div>
-                <div class="slide">
+                <div class="filme-bg">
                 <img src="/img/background/2.gif"/>
                 </div>
-                <div class="slide">
+                <div class="filme-bg">
                 <img src="/img/background/3.gif"/>
                 </div>
-                <div class="slide">
+                <div class="filme-bg">
                 <img src="/img/background/4.gif"/>
                 </div>
-                <div class="slide">
+                <div class="filme-bg">
                 <img src="/img/background/7.gif"/>
                 </div>
-                <div class="slide">
+                <div class="filme-bg">
                 <img src="/img/background/8.gif"/>
                 </div>
-                <div class="slide">
+                <div class="filme-bg">
                 <img src="/img/background/9.gif"/>
                 </div>
-                <div class="slide">
+                <div class="filme-bg">
                 <img src="/img/background/11.gif"/>
                 </div>
-                <div class="slide">
+                <div class="filme-bg">
                 <img src="/img/background/12.gif"/>
                 </div>
-                <div class="slide">
+                <div class="filme-bg">
                 <img src="/img/background/13.gif"/>
                 </div>
-                <div class="slide">
+                <div class="filme-bg">
                 <img src="/img/background/14.gif"/>
                 </div>
-                <div class="slide">
+                <div class="filme-bg">
                 <img src="/img/background/15.gif"/>
                 </div>
-                <div class="slide">
+                <div class="filme-bg">
                 <img src="/img/background/16.gif"/>
                 </div>
-                <div class="slide">
+                <div class="filme-bg">
                 <img src="/img/background/17.gif"/>
                 </div>
-                <div class="slide">
+                <div class="filme-bg">
                 <img src="/img/background/18.gif"/>
                 </div>
-                <div class="slide">
+                <div class="filme-bg">
                 <img src="/img/background/19.gif"/>
                 </div>
-                <div class="slide">
+                <div class="filme-bg">
                 <img src="/img/background/20.gif"/>
                 </div>
 
-                <div class="slide">
+                <div class="filme-bg">
                 <img src="/img/background/1.gif"/>
                 </div>
-                <div class="slide">
+                <div class="filme-bg">
                 <img src="/img/background/2.gif"/>
                 </div>
-                <div class="slide">
+                <div class="filme-bg">
                 <img src="/img/background/3.gif"/>
                 </div>
-                <div class="slide">
+                <div class="filme-bg">
                 <img src="/img/background/4.gif"/>
                 </div>
-                <div class="slide">
+                <div class="filme-bg">
                 <img src="/img/background/7.gif"/>
                 </div>
-                <div class="slide">
+                <div class="filme-bg">
                 <img src="/img/background/8.gif"/>
                 </div>
-                <div class="slide">
+                <div class="filme-bg">
                 <img src="/img/background/9.gif"/>
                 </div>
-                <div class="slide">
+                <div class="filme-bg">
                 <img src="/img/background/11.gif"/>
                 </div>
-                <div class="slide">
+                <div class="filme-bg">
                 <img src="/img/background/12.gif"/>
                 </div>
-                <div class="slide">
+                <div class="filme-bg">
                 <img src="/img/background/13.gif"/>
                 </div>
-                <div class="slide">
+                <div class="filme-bg">
                 <img src="/img/background/14.gif"/>
                 </div>
-                <div class="slide">
+                <div class="filme-bg">
                 <img src="/img/background/15.gif"/>
                 </div>
-                <div class="slide">
+                <div class="filme-bg">
                 <img src="/img/background/16.gif"/>
                 </div>
-                <div class="slide">
+                <div class="filme-bg">
                 <img src="/img/background/17.gif"/>
                 </div>
-                <div class="slide">
+                <div class="filme-bg">
                 <img src="/img/background/18.gif"/>
                 </div>
-                <div class="slide">
+                <div class="filme-bg">
                 <img src="/img/background/19.gif"/>
                 </div>
-                <div class="slide">
+                <div class="filme-bg">
                 <img src="/img/background/20.gif"/>
                 </div>
         </div>
