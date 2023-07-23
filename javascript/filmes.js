ba4a99735b59273d9e55cafcf657ec3b84aa394b
Tamiris Norbiato <tamiris.norbiato@outlook.com>
2023-07-23 17:54:06 -0300

    adição do botão adicionar à minha lista

---
+++ javascript/filmes.js
@@ -0,0 +1,25 @@
+function toggleClass1(){
+    let btnList = document.getElementById('btnList1');
+    btnList.classList.toggle('btnAdded');
+};
+
+function toggleClass2(){
+    let btnList = document.getElementById('btnList2');
+    btnList.classList.toggle('btnAdded');
+};
+
+function toggleClass3(){
+    let btnList = document.getElementById('btnList3');
+    btnList.classList.toggle('btnAdded');
+};
+
+function toggleClass4(){
+    let btnList = document.getElementById('btnList4');
+    btnList.classList.toggle('btnAdded');
+};
+
+function toggleClass5(){
+    let btnList = document.getElementById('btnList5');
+    btnList.classList.toggle('btnAdded');
+};
+
